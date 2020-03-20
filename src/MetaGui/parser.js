4e39876f771bea394293ae8cbdc639e0faafe40a
SB <ua9msn@gmail.com>
2020-03-20 11:45:08 +0300

    wip

--- src/MetaGui/parser.js
+++ src/MetaGui/parser.js
@@ -1,41 +1,18 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 const TYPES = Object.freeze({
     LABEL:  'label',
     TEXT:   'text',
     SUBMIT: 'submit'
 });
+
 const stringMatcher = new RegExp(/^".+"$/);
 
-function tokenize(code) {
-    return code
-        .split('\n')
-        .map(line => {
-            const [type, param = ''] = line.split(':').map(x => x.trim());
-            return {type, param}
-        })
-        .map(({type, param}) => {
-        switch (type) {
-            case TYPES.LABEL:
-                return props => <LabelElement value={param} {...props} />;
-            case TYPES.TEXT:
-                const isFormula = param.charAt(0) === '=';
-                return isFormula
-                    ? props => <ExpressionElement formula={param} {...props} />
-                    : props => <TextElement name={param} {...props} />;
-            case TYPES.SUBMIT:
-                return props => <SubmitElement value={param} {...props} />;
-            default:
-                return () => <NilEl/> ;
-        }
-    });
-}
-
-const NilEl = props => <div/>;
 const LabelElement = props => <label>{props.value}</label>;
-const TextElement = props => <input name={props.name} value={props.data[props.name] || ''} onChange={props.onChange} />;
+const TextElement = props => <input name={props.name} value={props.data[props.name] || ''} onChange={props.onChange}/>;
 const SubmitElement = props => <input type="submit" value={props.value || 'submit'}/>;
-const ExpressionElement = ({data, formula}) => {
+const ExpressionElement = ({data = {}, formula}) => {
     const tokens = formula
         .trim()
         .substr(1)
@@ -45,46 +22,75 @@ const ExpressionElement = ({data, formula}) => {
     tokens.forEach(t => {
         const isString = stringMatcher.test(t);
         text += isString
-            ? t.slice(1, -1)
-            : data && data[t] || ''
+            ? t.slice(1, -1)  // remove quotes
+            : data[t] || ''
     });
     return (
-        <input type="text" value={text} readOnly />
+        <input type="text" value={text} readOnly/>
     )
 };
 
 export class MetaForm extends Component {
+
+    static propTypes = {
+        code: PropTypes.string
+    };
+
     constructor(props) {
         super(props);
         this.state = {
-            data: {},
+            data:     {},
             elements: [],
-            code: ''
+            code:     ''
         }
     }
 
-    static getDerivedStateFromProps(props, state){
-        if(props.code === state.code) return null;
-        const elements = tokenize(props.code);
-        return {elements, code: props.code}
+    static tokenize(code) {
+        return code
+            .split('\n')
+            .map(line => {
+                const [type, param = ''] = line.split(':').map(x => x.trim());
+                return {type, param}
+            })
+            .map(({type, param}) => {
+                switch (type) {
+                    case TYPES.LABEL:
+                        return props => <LabelElement value={param} {...props} />;
+                    case TYPES.TEXT:
+                        const isFormula = param.charAt(0) === '=';
+                        return isFormula
+                            ? props => <ExpressionElement formula={param} {...props} />
+                            : props => <TextElement name={param} {...props} />;
+                    case TYPES.SUBMIT:
+                        return props => <SubmitElement value={param} {...props} />;
+                    default:
+                        return () => null;
+                }
+            });
     }
 
+    static getDerivedStateFromProps(props, state) {
+        if (props.code === state.code) return null;
+        const elements = MetaForm.tokenize(props.code);
+        return {
+            data: {}, // kill previously entered names
+            elements,
+            code: props.code
+        }
+    }
     onElChange = e => {
         e.stopPropagation();
         const data = {
             ...this.state.data,
-            [e.target.name]:  e.target.value
+            [e.target.name]: e.target.value
         };
         this.setState({data});
     };
-
     render() {
-
-        const Elements = this.state.elements.map((El, index) => {
-            return (
-                <El onChange={this.onElChange} data={this.state.data} key={index} />
-            )
-        });
+        const Elements = this.state.elements
+            .map((El, index) =>
+                <El onChange={this.onElChange} data={this.state.data} key={index}/>
+            );
         return (
             <form>
                 {Elements}
